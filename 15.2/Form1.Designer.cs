384b4aedbfc819f9d5e00758756bf63ff66d3fe2
pashtet482 <dydaevmakx41@gmail.com>
2024-05-06 21:49:25 +0300

    дождик

--- 15.2/Form1.Designer.cs
+++ 15.2/Form1.Designer.cs
@@ -28,32 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.canvasPictureBox = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.canvasPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // button1
             // 
-            this.textBox1.Location = new System.Drawing.Point(369, 122);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(299, 20);
-            this.textBox1.TabIndex = 0;
+            this.button1.Location = new System.Drawing.Point(684, 369);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 69);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // canvasPictureBox
+            // 
+            this.canvasPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.canvasPictureBox.Name = "canvasPictureBox";
+            this.canvasPictureBox.Size = new System.Drawing.Size(666, 426);
+            this.canvasPictureBox.TabIndex = 1;
+            this.canvasPictureBox.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.canvasPictureBox);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.canvasPictureBox)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox canvasPictureBox;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- 15.2/Form1.cs
+++ 15.2/Form1.cs
@@ -15,6 +15,41 @@ namespace _15._2
         public Form1()
         {
             InitializeComponent();
+            bmp = new Bitmap(canvasPictureBox.Width, canvasPictureBox.Height);
+            g = Graphics.FromImage(bmp);
+            rand = new Random();
+        }
+
+        private Bitmap bmp;
+        private Graphics g;
+        private Random rand;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DrawRaindrop();
+            timer1.Start();
+        }
+
+        private void DrawRaindrop()
+        {
+            // Закрашиваем фон
+            g.Clear(Color.White);
+
+            // Рисуем дождь
+            Pen pen = new Pen(Color.Blue, 1);
+            for (int i = 0; i < 100; i++)
+            {
+                int x = rand.Next(0, canvasPictureBox.Width);
+                int y = rand.Next(0, canvasPictureBox.Height);
+                g.DrawLine(pen, x, y, x, y + 5);
+            }
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DrawRaindrop();
+            canvasPictureBox.Image = bmp;
         }
     }
 }
